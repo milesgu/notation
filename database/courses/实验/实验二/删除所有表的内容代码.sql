7ff2fe8774c185ba0a2049d71b99b95e47362a36
MilesGO <164173218@qq.com>
2018-04-02 19:36:16 +0800

    2018/04/02 周一 19:36:16.04

---
+++ database/courses/实验/实验二/删除所有表的内容代码.sql
@@ -0,0 +1,10 @@
+use student_class_database
+delete from book;
+delete from class;
+delete from class_course;
+delete from course;
+delete from department;
+delete from student;
+delete from student_course;
+delete from teacher;
+delete from teacher_course_class;
--- database/courses/实验/实验二/表操作代码.sql
+++ database/courses/实验/实验二/表操作代码.sql
@@ -1,3 +1,54 @@
+--从excel导入数据到已有表格
+INSERT INTO book
+SELECT *
+FROM OPENROWSET('MICROSOFT.JET.OLEDB.4.0', 'EXCEL 5.0;HDR=YES;DATABASE=C:\Users\16417\Desktop\notation\database\courses\实验\实验二\数据\book.xls', sheet1$)
+
+--向course表中增加一条记录
+USE student_class_database  
+INSERT INTO course VALUES('dep04_j001','计算机系统结构','dep04_j001_02',68,4,6);  
+SELECT *  
+FROM course;
+
+--从course表中删除一条记录
+USE student_class_database
+DELETE FROM course WHERE course_id='dep04_j001';
+SELECT *
+FROM course;
+
+--在course表中修改课名为计算机导论的credit属性取值为5
+USE student_class_database  
+UPDATE course SET credit=5 WHERE course_name='计算机导论'  
+SELECT *  
+FROM course;  
+
+--向class表中增加一条记录,monitor属性取值为空
+USE student_class_database
+INSERT INTO class (class_id,classroom,department_id) 
+VALUES ('g00404','教学楼313','dep_01');
+SELECT *
+FROM class;
+
+--从class表中删除所有department_id属性取值为'dep_01'的记录
+USE student_class_database
+DELETE FROM class WHERE department_id='dep_01';
+SELECT *
+FROM class;
+
+--class_course表操作SQL语句
+USE student_class_database
+INSERT INTO class_course(class_id,course_id)(
+		SELECT 'g99401',T.course_id
+		FROM class_course AS T
+		WHERE T.class_id='g99402'
+);
+
+DELETE FROM class_course
+WHERE class_id='g99402';
+
+UPDATE class_course
+SET class_id='g99402'
+WHERE class_id='g99403'
+
 -- department表操作SQL语句
 use student_class_database
 
